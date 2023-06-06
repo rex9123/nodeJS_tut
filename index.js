32b1d974b35f69a3e3a8e88e19f795c97f505e3e
Rex <mariarexswamy121@gmail.com>
2023-06-06 23:51:04 +0530

    CRUD operations in file systems

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const { dir } = require('console')
 const fs = require('fs')
 const path = require('path')
 const dirPath = path.join(__dirname,'files')
-for(i=0;i<5;i++)
-{
-    fs.writeFileSync(dirPath+`hello`+i+`.txt` , "a simple text file")
-}
-console.log(dirPath)
+const filepath = `${dirPath}/apple.txt`
+    
+/*fs.writeFileSync(filepath,'This is a simple text file')
+fs.readFile(filepath , (err,item) => {
+    console.log(item)
+})
+
+fs.appendFile(filepath,' and file name is apple.txt',(err)=> {
+    if(!err) console.log('File is updated')
+})
+
+fs.rename(filepath,`${dirPath}/fruit.txt`,(err) => {
+        if(!err) console.log("file name is updated")
+})
+*/
+fs.unlinkSync(`${dirPath}/fruit.txt`)
