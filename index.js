531bf14d349b1cbc5fbd2b9087d77d3b7e7c4ec3
Rex <mariarexswamy121@gmail.com>
2023-06-10 18:20:09 +0530

    Started Express

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-// imageLoad.js
-const loadImage = new Promise((resolve, reject) => {
-    const image = new Image();
-    image.src = 'C:/Users/maria/OneDrive/Pictures/Screenshots/Screenshot 2023-05-27 212557.png';
-  
-    image.onload = () => {
-      resolve(image); // Resolve the promise with the loaded image
-    };
-  
-    image.onerror = () => {
-      reject('Failed to load image'); // Reject the promise with an error message
-    };
-  });
-  
-  loadImage
-    .then((image) => {
-      // Do something with the loaded image
-      console.log('Image loaded:', image);
-    })
-    .catch((error) => {
-      // Handle the error if the image failed to load
-      console.error('Image loading failed:', error);
-    });
-  
+const express = require('express')
+const app = express();
+
+app.get('',(req,res) => {
+    res.send('Hello , this is Home Page')
+})
+
+app.get('/help',(req,res) => {
+  res.send('Hello , this is Help Page')
+})
+
+app.listen(5500)
