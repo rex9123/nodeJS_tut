5b4ddceabdc9658c51fe1ca4fb5e8760052036b8
Rex <mariarexswamy121@gmail.com>
2023-06-12 20:58:14 +0530

    Methods and Routes advanced...

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express();
 
 app.get('',(req,res) => {
-    res.send('Hello , this is Home Page')
+    res.send(`<input type = "text" placeholder = "User name" value= " ${req.query.name}" />
+    <button>Click</button>
+    <a href = "/help" >Go to help page</a>`)
 })
 
 app.get('/help',(req,res) => {
-  res.send('Hello , this is Help Page')
+  res.send(`<h1>Any help</h1>`)
 })
 
 app.listen(5500)
