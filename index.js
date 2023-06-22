1e487a6a403f4a3a2629974c16e52d2e4eb81327
Rex <mariarexswamy121@gmail.com>
2023-06-22 22:39:21 +0530

    Middleware

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
+const reqFilter = (req,res,next) =>{
+    //console.log('reqFilter')
+
+    if(!req.query.age){
+        res.send('Please provide age')
+    }
+    else if(req.query.age === 0 || req.query.age < 18){
+        res.send('ACESS DENIED !')
+    }
+    else{
+    next();
+    }
+}
+
+
+app.use(reqFilter)
 
 app.get('/',(req,res) => {
     res.send('<h1>Welcome to Home page</h1>')
 })
+
 
 app.get('/users',(req,res) => {
     res.send('<h1>Welcome to Users page </h1>')
