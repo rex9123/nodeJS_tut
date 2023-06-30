92158b8d3d055e81dc38996a9f0ed9b87e447865
Rex <mariarexswamy121@gmail.com>
2023-06-30 16:09:32 +0530

    Some random changes..

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 const express = require('express')
 const app = express()
-const reqFilter = require('./middleware')
-const route = express.Router();
 
-route.use(reqFilter)
-app.get('/',(req,res) => {
-    res.send('<h1>Welcome to Home page</h1>')
+const route = express.Router()
+
+app.get('',(req,res)=> {
+    res.send('<h1>Welcome to home page</h1>')
 })
-
-
-app.get('/users',reqFilter,(req,res) => {
-    res.send('<h1>Welcome to Users page </h1>')
-})
-
-route.get('/about',(req,res) => {
-    res.send('<h1>Welcome to About page </h1>')
-})
-
-route.get('/contact',(req,res) => {
-    res.send('<h1>Welcome to Contact page </h1>')
-})
-
-
-app.use('/',route)
-
-
-
-
 app.listen(5500)
 console.log("Server running on port 5500")
