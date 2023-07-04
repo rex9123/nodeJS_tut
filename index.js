3152fecc7a5da16b3d6ae64e4d8258d9f38c0eaa
Rex <mariarexswamy121@gmail.com>
2023-07-04 16:09:13 +0530

    MongoDB connection

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const { MongoClient } = require("mongodb");
 const url = "mongodb://0.0.0.0:27017";
+const express = require('express')
+const app = express()
 const database = "ecomm";
 const client = new MongoClient(url);
 
 async function getData() {
-  let result = await client.connect();
-  let db = result.db(database);
-  let collection = db.collection("products");
-  let response = await collection.find({}).toArray();
-  console.log(response);
-}
-getData();
+    try {
+      await client.connect();
+      const db = client.db(database);
+      const collection = db.collection("products");
+      const response = await collection.find({}).toArray();
+      return response;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    } finally {
+      client.close();
+    }
+  }
+  
+  app.get('/', async (req, res) => {
+    try {
+      const data = await getData();
+      res.send(JSON.stringify(data));
+    } catch (error) {
+      res.status(500).send('Error retrieving data');
+    }
+  });
+  
+  app.listen(5500, () => {
+    console.log('Server started on port 5500');
+  });
+
